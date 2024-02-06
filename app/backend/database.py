9a0065219e89f33a94b5c6d39c193d12c1deb4d3
Maxim Ivanov <moxeeeem@gmail.com>
2024-02-06 22:32:30 +0300

    update database.py

--- app/backend/database.py
+++ app/backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-URL = "postgresql://moxeeem:JSqcgw1DEr4P@ep-rough-water-90638384.eu-central-1.aws.neon.tech/bankcustomers"
+URL = "DATABASE_URL"
 
 engine = create_engine(URL)
 
